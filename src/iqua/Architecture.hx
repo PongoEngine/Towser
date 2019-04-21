486b282d03b0018af18f2991ed9556f4b8d0395d
Jeremy <cream@sourcre.am>
2019-04-20 18:10:11 -0700

    payloading added to mouse event

--- src/iqua/Architecture.hx
+++ src/iqua/Architecture.hx
@@ -34,8 +34,21 @@ class Architecture<Model, Msg>
 
         for(a in attributes) {
             switch a {
-                case ON_DBL_CLICK(msg): setField(obj.on, "dblclick", arch.update.bind(msg));
+                //MOUSE EVENTS
                 case ON_CLICK(msg): setField(obj.on, "click", arch.update.bind(msg));
+                case ON_DBL_CLICK(msg): setField(obj.on, "dblclick", arch.update.bind(msg));
+                case AUX_CLICK(msg): setField(obj.on, "auxclick", arch.update.bind(msg));
+                case CONTEXT_MENU(msg): setField(obj.on, "contextmenu", arch.update.bind(msg));
+                case SELECT(msg): setField(obj.on, "select", arch.update.bind(msg));
+                case WHEEL(msg): setField(obj.on, "wheel", arch.update.bind(msg));
+                case MOUSE_DOWN(d,f): setField(obj.on, "mousedown", (e) -> arch.update(f(d,e.pageX, e.pageY)));
+                case MOUSE_ENTER(d,f): setField(obj.on, "mouseenter", (e) -> arch.update(f(d,e.pageX, e.pageY)));
+                case MOUSE_LEAVE(d,f): setField(obj.on, "mouseleave", (e) -> arch.update(f(d,e.pageX, e.pageY)));
+                case MOUSE_MOVE(d,f): setField(obj.on, "mousemove", (e) -> arch.update(f(d,e.pageX, e.pageY)));
+                case MOUSE_OVER(d,f): setField(obj.on, "mouseover", (e) -> arch.update(f(d,e.pageX, e.pageY)));
+                case MOUSE_OUT(d,f): setField(obj.on, "mouseout", (e) -> arch.update(f(d,e.pageX, e.pageY)));
+                case MOUSE_UP(d,f): setField(obj.on, "mouseup", (e) -> arch.update(f(d,e.pageX, e.pageY)));
+
                 case ON_CHANGE(f): setField(obj.on, "change", (e) -> arch.update(f(e.target.value)));
                 case ON_INPUT(f): setField(obj.on, "input", (e) -> arch.update(f(e.target.value)));
                 case CLASS(value): setField(obj.attrs, "class", value);
--- src/iqua/Attribute.hx
+++ src/iqua/Attribute.hx
@@ -4,8 +4,22 @@ import iqua.Architecture;
 
 enum Attribute<Msg>
 {
+    //MOUSE EVENTS
     ON_CLICK(msg :Msg);
     ON_DBL_CLICK(msg :Msg);
+    AUX_CLICK(msg :Msg);
+    CONTEXT_MENU(msg :Msg);
+    SELECT(msg :Msg);
+    WHEEL(msg :Msg);
+
+    MOUSE_DOWN(data :Dynamic, f :Dynamic -> Int -> Int -> Msg);
+    MOUSE_ENTER(data :Dynamic, f :Dynamic -> Int -> Int -> Msg);
+    MOUSE_LEAVE(data :Dynamic, f :Dynamic -> Int -> Int -> Msg);
+    MOUSE_MOVE(data :Dynamic, f :Dynamic -> Int -> Int -> Msg);
+    MOUSE_OVER(data :Dynamic, f :Dynamic -> Int -> Int -> Msg);
+    MOUSE_OUT(data :Dynamic, f :Dynamic -> Int -> Int -> Msg);
+    MOUSE_UP(data :Dynamic, f :Dynamic -> Int -> Int -> Msg);
+
     ON_CHANGE(f :Dynamic -> Msg);
     ON_INPUT(f :Dynamic -> Msg);
     CLASS(value :String);
