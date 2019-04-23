fbf1cff0cecf22712ae0a8538af852bc7e88c4e6
Jeremy <cream@sourcre.am>
2019-04-23 12:23:42 -0700

    Update IncrementalDOM.hx

--- src/iqua/IncrementalDOM.hx
+++ src/iqua/IncrementalDOM.hx
@@ -1,16 +1,23 @@
 package iqua;
 
+import haxe.Constraints.Function;
+import haxe.extern.Rest;
+import js.html.Element;
+import js.html.Node;
+
 @:jsRequire("incremental-dom")
 extern class IncrementalDOM
 {
-    public static function elementOpen() : Dynamic;
+    public static function elementOpen(tagname :String, key :String, staticPropertyValuePairs :Array<String>, vargs :Rest<Dynamic>) : Element;
+    public static function elementClose(tagname :String) : Element;
+    public static function patch(node :Node, description :Function, data :Any) : Void;
+
+
     public static function elementOpenStart() : Dynamic;
     public static function attr() : Dynamic;
     public static function elementOpenEnd() : Dynamic;
-    public static function elementClose() : Dynamic;
     public static function elementVoid() : Dynamic;
     public static function text() : Dynamic;
-    public static function patch() : Dynamic;
     public static function currentElement() : Dynamic;
     public static function currentPointer() : Dynamic;
     public static function skip() : Dynamic;
