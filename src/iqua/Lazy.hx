70b5d2917bb163f53fc78fd898b5b127337ea83d
Jeremy <cream@sourcre.am>
2019-04-23 22:44:30 -0700

    Update Lazy.hx

--- src/iqua/Lazy.hx
+++ src/iqua/Lazy.hx
@@ -10,22 +10,22 @@ typedef Lazy3<Model, Msg, A, B, C> = Architecture<Model, Msg> -> A -> B -> C -> 
 
 class Lazy
 {
-    public static function lazy1<Model, Msg, A>(selector :String, f :Lazy1<Model, Msg, A>) : Lazy1<Model, Msg, A>
+    public static inline function lazy1<Model, Msg, A>(selector :String, f :Lazy1<Model, Msg, A>) : Lazy1<Model, Msg, A>
     {
         return _lazy1.bind(selector, f);
     }
 
-    public static function lazy2<Model, Msg, A, B>(selector :String, f :Lazy2<Model, Msg, A, B>) : Lazy2<Model, Msg, A, B>
+    public static inline function lazy2<Model, Msg, A, B>(selector :String, f :Lazy2<Model, Msg, A, B>) : Lazy2<Model, Msg, A, B>
     {
         return _lazy2.bind(selector, f);
     }
 
-    public static function lazy3<Model, Msg, A, B, C>(selector :String, f :Lazy3<Model, Msg, A, B, C>) : Lazy3<Model, Msg, A, B, C>
+    public static inline function lazy3<Model, Msg, A, B, C>(selector :String, f :Lazy3<Model, Msg, A, B, C>) : Lazy3<Model, Msg, A, B, C>
     {
         return _lazy3.bind(selector, f);
     }
 
-    public static function _lazy1<Model, Msg, A>(selector :String, f :Lazy1<Model, Msg, A>, arch :Architecture<Model, Msg>, a :A) : RenderFunction<Model, Msg>
+    private static function _lazy1<Model, Msg, A>(selector :String, f :Lazy1<Model, Msg, A>, arch :Architecture<Model, Msg>, a :A) : RenderFunction<Model, Msg>
     {
         if(!arch._lazyMap.shouldSkip1(selector, a)) {
             arch._lazyMap.setLazy1(selector, a);
@@ -36,7 +36,7 @@ class Lazy
         }
     }
 
-    public static function _lazy2<Model, Msg, A, B>(selector :String, f :Lazy2<Model, Msg, A, B>, arch :Architecture<Model, Msg>, a :A, b:B) : RenderFunction<Model, Msg>
+    private static function _lazy2<Model, Msg, A, B>(selector :String, f :Lazy2<Model, Msg, A, B>, arch :Architecture<Model, Msg>, a :A, b:B) : RenderFunction<Model, Msg>
     {
         if(!arch._lazyMap.shouldSkip2(selector, a, b)) {
             arch._lazyMap.setLazy2(selector, a, b);
@@ -47,7 +47,7 @@ class Lazy
         }
     }
 
-    public static function _lazy3<Model, Msg, A, B, C>(selector :String, f :Lazy3<Model, Msg, A, B, C>, arch :Architecture<Model, Msg>, a :A, b:B, c :C) : RenderFunction<Model, Msg>
+    private static function _lazy3<Model, Msg, A, B, C>(selector :String, f :Lazy3<Model, Msg, A, B, C>, arch :Architecture<Model, Msg>, a :A, b:B, c :C) : RenderFunction<Model, Msg>
     {
         if(!arch._lazyMap.shouldSkip3(selector, a, b, c)) {
             arch._lazyMap.setLazy3(selector, a, b, c);
